1dc74b77e4cf49bc3e953018f02a493a7c8911cd
Leon Zhang <leonz5288@hotmail.com>
2023-10-11 22:08:09 -0400

    fs server setup

--- dns_app/US/us.py
+++ dns_app/US/us.py
@@ -2,14 +2,16 @@ from flask import Flask, request
 
 app = Flask('user_server')
 
-@app.route('/fibonacci')
+@app.route('/fibonacci', method=['GET'])
 def ask_for_num():
     hostname = request.args.get('hostname')
     fs_port = request.args.get('fs_port')
     number = request.args.get('number')
     as_ip = request.args.get('as_ip')
     as_port = request.args.get('as_port')
-    return str(hostname) + str(as_ip)
+    if hostname is None or fs_port is None or number is None or as_ip is None or as_port is None:
+        return 'Lack of Parameters', 400
+    return str(number), 200
 
 if __name__ == '__main__':
     app.run(port=8080)
